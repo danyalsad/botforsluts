f7d205fb96c951f967d227d0b53b35fb3a853170
danyalsad <45831319+danyalsad@users.noreply.github.com>
2019-10-26 16:14:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const reddit = new snoowrap({
 // Our main function that will call the Reddit api for new stories in a specific subreddit
 // if you aren't familiar with ES6 syntax, the sub will default to /r/aww, the number of stories to 10.
 // The arrows are one way of writing functions in ES6.
-getNewStories = (sub='CheatingSluts', num=10) => {
+getNewStories = (sub='CheatingSluts', num=1) => {
     reddit.getSubreddit(sub).getHot()
     .then(function(listing) {
         for (var i = 0; i < num ; i++) {
@@ -57,14 +57,14 @@ postNewStory = (post) => {
 
 // This function runs getNewStories in several diffrent subreddits and posts them in our subreddit
 getDevStories = () => {
-  getNewStories('collegesluts', 10);
-  getNewStories('sluts', 10);
-  getNewStories('SluttySelfies', 10);
-  getNewStories('tinder_sluts', 10);
-  getNewStories('fuckyeahsexyteens', 10);
-  getNewStories('barelylegalteens', 10);
-  getNewStories('LegalTeens', 10);
-  getNewStories('RealGirls', 10);
+  getNewStories('collegesluts', 1);
+  getNewStories('sluts', 1);
+  getNewStories('SluttySelfies', 1);
+  getNewStories('tinder_sluts', 1);
+  getNewStories('fuckyeahsexyteens', 1);
+  getNewStories('barelylegalteens', 1);
+  getNewStories('LegalTeens', 1);
+  getNewStories('RealGirls', 1);
 }
 
 
